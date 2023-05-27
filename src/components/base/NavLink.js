bd4c4517a9287744d628cebdbbb158d8c89b66af
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-27 19:04:01 +0300

    feat: added padding to links

--- src/components/base/NavLink.js
+++ src/components/base/NavLink.js
@@ -1,7 +1,7 @@
 export default function NavLink({ to, children }){
 
   return(
-    <a href={to} className='text-white/70 hover:text-white transition-all duration-300 flex items-center gap-x-5 h-10 font-semibold capitalize text-base'>
+    <a href={to} className='text-white/70 hover:text-white transition-all duration-300 flex items-center gap-x-5 h-10 font-semibold capitalize text-base px-1 py-3'>
       {children}
     </a>
   );
