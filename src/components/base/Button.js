f4772a07aaca232255cbe95cedb319fe9b779ef7
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-28 19:12:05 +0300

    fix: moved buttons styles into another component

--- src/components/base/Button.js
+++ src/components/base/Button.js
@@ -1,28 +1,12 @@
-import classNames from 'classnames'
 import Icon from './Icon';
 
-export default function Button({ children ,className, highlight, elevated, type, iconSize, scale }){
+export default function Button({ children ,className, type, iconSize, }){
 
-  const finalClassnames = classNames('rounded-full p-2 text-white/70 hover:text-white transition-all', className, {
-    'hover:bg-highlight': highlight,
-    'bg-elevated-base hover:bg-elevated-highlight': elevated,
-    'hover:scale-105': scale,
-  })
 
   return(
-    <button className={finalClassnames}>
+    <button className={className}>
       {type && <Icon type={type} size={iconSize} />}
       {children}
     </button>
   );
-}
-
-Button.propTypes = {
-  checkVariations: ({ highlight, elevated }) => {
-    const count = Number(!!highlight) + Number(!!elevated)
-  
-    if(count > 1) {
-      return new Error('only one type can be true')
-    }
-  },
 }
