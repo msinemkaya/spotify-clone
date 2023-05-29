aca287afe48eb2c2cf37a5cd7d715ede7d4c0cda
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-29 20:57:25 +0300

    feat: added some styling to dropdowb

--- src/components/common/Dropdown.js
+++ src/components/common/Dropdown.js
@@ -24,13 +24,13 @@ export default function Dropdown({ options, selection, onSelect }){
     <Container ref={dropdown} className={'relative'}>
       <Button className={'text-xs text-white/70 hover:text-white flex gap-2 h-8 items-center px-2'} onClick={handleClick}>
         {selection?.label || 'Yakın tarihli'}
-        <Icon type={'down'} />
+        {isOpen ? <Icon type={'up'} /> : <Icon type={'down'} />}
       </Button>
       {isOpen && (
         <Panel>
           <H6 className={'text-[11px] font-semibold text-white/70 py-3 pr-2 pl-3'}>Şuna göre sırala:</H6>
           {options.map(option => (
-            <Button key={option.key} onClick={() => handleOptionClick(option)} className={`min-w-[10rem] flex py-3 pr-2 pl-3 text-sm hover:bg-white/10 rounded-sm justify-between ${option.label === selection?.label ? 'text-green-700': null}`}>
+            <Button key={option.key} onClick={() => handleOptionClick(option)} className={`min-w-[10rem] flex py-3 pr-2 pl-3 text-sm hover:bg-white/10 rounded-sm justify-between focus:outline-none ${option.label === selection?.label ? 'text-green-700': null}`}>
               {option.label}
               {option.label === selection?.label && <Icon type={'check'} size={22}/> }
             </Button>
