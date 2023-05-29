bf1eaa5989afe7d8a7f71eeea658619bc1e96f57
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-29 17:21:27 +0300

    feat: created slider menu

---
+++ src/components/common/SliderMenu.js
@@ -0,0 +1,53 @@
+import SpotifyButton from './SpotifyButton';
+import FlexBox from './FlexBox';
+import Container from 'components/base/Container';
+import { useEffect, useRef, useState } from 'react';
+
+export default function SliderMenu({ children }){
+
+  const [scrollX, setScrollX] = useState(0)
+  const [scrolEnd, setscrolEnd] = useState(false)
+
+  const scroll = useRef(null)
+
+  const items = [
+    'çalma listeleri',
+    'sanatçılar',
+    'albümler',
+    `podcast'ler ve programlar`
+  ]
+
+  useEffect(() => {
+    if (
+      scroll.current &&
+      scroll?.current?.scrollWidth === scroll?.current?.offsetWidth
+    ) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+    return () => {};
+  }, [scroll?.current?.scrollWidth, scroll?.current?.offsetWidth]);
+
+  const handleScroll = (e) => {
+    setScrollX(scroll.current.scrollLeft);
+    if (
+      Math.floor(scroll.current.scrollWidth - scroll.current.scrollLeft) <=
+      scroll.current.offsetWidth
+    ) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+  }
+
+  return(
+    <Container className={'relative py-2'}>
+      {scrollX !== 0 && <SpotifyButton elevated type={'left'} className={'z-10 absolute top-2 left-0'}  />}
+        <FlexBox className={'flex gap-2 items-center overflow-auto no-scrollbar slider-shadow'} onScroll={handleScroll} ref={scroll}>
+          {items.map((item, index)=> <SpotifyButton key={index} elevated className={'!text-white h-8 whitespace-nowrap text-sm py-1 px-3 capitalize'}>{item}</SpotifyButton>)}
+        </FlexBox>
+      {!scrolEnd && <SpotifyButton elevated type={'right'} className={'z-10 absolute top-2 right-0'}/>}
+    </Container>
+  );
+}
--- src/components/sections/SideBar.js
+++ src/components/sections/SideBar.js
@@ -1,6 +1,8 @@
 import Button from 'components/base/Button'
 import Container from 'components/base/Container'
+import FlexBox from 'components/common/FlexBox'
 import NavLink from 'components/common/NavLink'
+import SliderMenu from 'components/common/SliderMenu'
 import SpotifyButton from 'components/common/SpotifyButton'
 import Wrapper from 'components/common/Wrapper'
 import { AiOutlineHome } from 'react-icons/ai'
@@ -23,24 +25,27 @@ export default function SideBar({}) {
   return (
     <Container className={'flex flex-col gap-2 w-90'}>
       <Wrapper>
-        {navLinks.map((link) => (
-          <NavLink to={link.to}>
+        {navLinks.map((link, index) => (
+          <NavLink key={index} to={link.to}>
             {link.icon}
             <span>{link.text}</span>
           </NavLink>
         ))}
       </Wrapper>
-      <Wrapper className={'flex items-center justify-between'}>
-        <Button
-          className={
-            'text-white/70 hover:text-white transition-all duration-300 flex items-center gap-x-5 h-10 font-semibold capitalize text-base p-3'
-          }
-          type={'library'}
-          iconSize={24}
-        >
-          kitaplığın
-        </Button>
-        <SpotifyButton highlight type={'plus'} />
+      <Wrapper>
+        <FlexBox className={'justify-between py-2'}>
+          <Button
+            className={
+              'text-white/70 hover:text-white transition-all duration-300 flex items-center gap-x-5 h-10 font-semibold capitalize text-base p-3'
+            }
+            type={'library'}
+            iconSize={24}
+          >
+            kitaplığın
+          </Button>
+          <SpotifyButton highlight type={'plus'} />
+        </FlexBox>
+        <SliderMenu/>
       </Wrapper>
     </Container>
   )
