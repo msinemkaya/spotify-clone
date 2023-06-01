b29ee398a53c5d96858deeecf5b65b1b7cee737e
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-06-01 14:43:58 +0300

    feat: changed window location to useLocation hook

--- src/components/common/UserMenu.js
+++ src/components/common/UserMenu.js
@@ -1,16 +1,17 @@
 import Container from 'components/base/Container'
 import FlexBox from './FlexBox'
 import SpotifyButton from './SpotifyButton'
-import Wrapper from './Wrapper'
+import { useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import SearchInput from './SearchInput'
 import Span from 'components/base/Span'
 
 export default function UserMenu({}) {
   const [isSearch, setIsSearch] = useState(false)
+  const location = useLocation()
 
   useEffect(() => {
-    if (window.location.pathname === '/search') {
+    if (location.pathname === '/search') {
       setIsSearch(true)
     } else {
       setIsSearch(false)
