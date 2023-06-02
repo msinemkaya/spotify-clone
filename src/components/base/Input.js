8e036a5d76d9b3a581647410c5b7e6cf26c5d789
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-06-02 11:03:12 +0300

    fix: fixed import on input

--- src/components/base/Input.js
+++ src/components/base/Input.js
@@ -1,9 +1,10 @@
-const { forwardRef } = require('react')
+import { forwardRef } from 'react'
 
 const Input = forwardRef((props, ref) => {
   const { placeholder, className, type, ...rest} = props
   return (
     <input
+      ref={ref}
       type={type}
       placeholder={placeholder}
       className={className}
